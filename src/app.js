b0822458a8281204d9a284dbf94887b03cc23965
Mehul Patel <mehulpatel118@gmail.com>
2019-04-12 16:24:56 -0400

    finished

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request '+ apiToken });
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
   next();
 });
