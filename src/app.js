bc00b1d508e767d79e6f9d26d942f51e9c23d58b
Mehul Patel <mehulpatel118@gmail.com>
2019-04-12 15:25:45 -0400

    updated my app

--- src/app.js
+++ src/app.js
@@ -5,9 +5,25 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV,DB_URL } = require('./config');
+const noteRouter = require('./note-router');
+const folderRouter = require('./folder-router');
+const logger = require('./logger');
+
 
 const app = express();
+app.use(cors());
+app.use(helmet());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
 
 const morganOption = (NODE_ENV === 'production')
@@ -15,12 +31,15 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
-app.use(helmet());
+
+app.use('/api/notes',noteRouter);
+app.use('/api/folders',folderRouter);
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+  res.send(DB_URL);
 });
 
 app.use(function errorHandler(error, req, res, next) {
